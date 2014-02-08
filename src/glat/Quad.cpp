f952bf7ef32c7d038b472450576802dcae12c407
Daniel.Gimbatschki <Daniel.Gimbatschki@student.hpi.uni-potsdam.de>
2014-02-08 16:17:51 +0100

    bugfix: now correctly displays upper half of quad-rendered annotation

--- src/glat/Quad.cpp
+++ src/glat/Quad.cpp
@@ -21,7 +21,7 @@ glat::Quad::Quad(std::shared_ptr<glow::Texture> distanceField) : glat::AbstractD
 
 	m_vao->binding(1)->setAttribute(1);
 	m_vao->binding(1)->setBuffer(m_texCoords, 0, sizeof(glm::vec2));
-	m_vao->binding(1)->setFormat(4, GL_FLOAT, GL_FALSE, 0);
+	m_vao->binding(1)->setFormat(2, GL_FLOAT, GL_FALSE, 0);
 	m_vao->enable(1);
 
 	setPosition(glm::vec3(-1.f, -1.f, 0.f), glm::vec3(1.f, -1.f, 0.f), glm::vec3(1.f, 1.f, 0.f));
@@ -32,16 +32,17 @@ glat::Quad::Quad(std::shared_ptr<glow::Texture> distanceField) : glat::AbstractD
 
 void glat::Quad::setPosition(glm::vec3 ll, glm::vec3 lr, glm::vec3 ur, glm::mat4 modelViewProjection /*= glm::mat4()*/) {
 
-	std::array<glm::vec3, 4> vertexArray{ {
-			glm::vec3(lr.x, lr.y, lr.z)
-		,	glm::vec3(ur.x, ur.y, ur.z)
-		,	glm::vec3(ll.x, ll.y, ll.z)
-		,	glm::vec3(ll.x + (ur.x - lr.x), ll.y + (ur.y - lr.y), ll.z + (ur.z - lr.z))
+	std::array<glm::vec4, 4> vertexArray{ {
+				glm::vec4(lr.x, lr.y, lr.z, 1.0)
+			,	glm::vec4(ur.x, ur.y, ur.z, 1.0)
+			,	glm::vec4(ll.x, ll.y, ll.z, 1.0)
+			,	glm::vec4(ll.x + (ur.x - lr.x), ll.y + (ur.y - lr.y), ll.z + (ur.z - lr.z), 1.0)
+
 	}};
 
 
 	m_positions->setData(vertexArray, GL_STATIC_DRAW);
-	m_vao->binding(0)->setBuffer(m_positions, 0, sizeof(glm::vec3));
+	m_vao->binding(0)->setBuffer(m_positions, 0, sizeof(glm::vec4));
 
 	m_program->setUniform("modelViewProjection", modelViewProjection);
 }
--- src/glat/ShaderSources.hpp
+++ src/glat/ShaderSources.hpp
@@ -5,14 +5,14 @@ namespace glat {
 
 				uniform mat4 modelViewProjection;
 
-				layout (location = 0) in vec3 position;
+				layout (location = 0) in vec4 position;
 				layout (location = 1) in vec2 textureCoord;
 				out vec2 v_uv;
 
 				void main()
 				{
 					v_uv = textureCoord;
-					gl_Position = modelViewProjection * vec4(position, 1.0);
+					gl_Position = modelViewProjection * position;
 				}
 				)";
 
