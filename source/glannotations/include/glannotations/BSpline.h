ddbea21fcf69e1e04915b57cada26052cd78c8ee
anne.gropler <anne.gropler@student.hpi.uni-potsdam.de>
2015-05-21 11:45:26 +0200

    * added some functions to BSpline, copied (and adjusted) from Bachelor project 2013 "Bundle View" Annotation * not sure about 3D-splines and how to put the text on it, since this time I don't want to limit the splineAnnotation to the x-z-plane.

--- source/glannotations/include/glannotations/BSpline.h
+++ source/glannotations/include/glannotations/BSpline.h
@@ -17,6 +17,19 @@ namespace glannotations {
 		const std::vector<float>& getKnotValues();
 		unsigned short getSplineDegree();
 
+		/**
+		* calculates a Curvepoint parametrized by t using de Boor's algorithm
+		* t Curve parameter in range [0,1]
+		*/
+		glm::vec3 getCurvepointAt(float t);
+
+		/**
+		* Calculates 2D-vector between two curvepoints specified by curve parameters t and nextT, not normalized
+		* t Curve parameter for first curvepoint
+		* nextT Curve parameter for second curvepoint
+		*/
+		glm::vec2 getSecantVectorAt(float t, float nextT);
+
 		// we want to use the dirty information just internally
         //virtual bool isDirty() const = delete;
 
@@ -26,8 +39,26 @@ namespace glannotations {
 		void calculateSplineDegree();
 		void calculateUniformKnotValues();
 
+		/**
+		* Calculates values to fill the arcLength-container, which contains the arcLength at certain points along the curve,
+		* used to approximate the overall arcLength and to position the glyphs evenly along the curve
+		*/
+		void calculateArcLengths();
+
+		/**
+		* returns point of the curve at position t
+		* k the remaing searching depth, initial value == k
+		* degree the degree of the spline
+		* i position in knot vector
+		* t position in curve-range[0,controlPoints.length]
+		* knots defines the influence of the controlPoints
+		*/
+		glm::vec3 deBoor(int k, int degree, int i, float t, std::vector<float> knots);
+
 		std::vector<glm::vec3> m_ctrlPoints;
 		std::vector<float> m_knotValues;
 		unsigned short m_degree;
+
+		std::vector<float> m_arcLengths;
 	};
 }
--- source/glannotations/source/BSpline.cpp
+++ source/glannotations/source/BSpline.cpp
@@ -1,4 +1,5 @@
 #include <glannotations/BSpline.h>
+#include <iostream> //debug todo: remove
 
 const std::vector<glm::vec3>& glannotations::BSpline::getControlPoints() {
 	return m_ctrlPoints;
@@ -59,3 +60,78 @@ void glannotations::BSpline::calculateSplineDegree() {
 	setDirty(true);
 }
 
+void glannotations::BSpline::calculateArcLengths() {
+	//It may be possible to estimate the error and dynamically pick an "ideal" number of divisions.
+	//This could be done by subdividing until segment lengths are below some threshold or
+	//by comparing the tangents of the line segments to the tangents of the curve,
+	//but I have not explored either of these ideas.
+	//Fixing the number of divisions at around 100 seems to work well enough for the purpose at hand. 
+	//UPDATE: the longer the curve, the higher numberOfSubdivisions needed
+	//& the longer an icicle, the more curve points given
+	// => the more curve points, the higher numberOfSubdivisions!
+	int numberOfDivisions = 10 * m_ctrlPoints.size();
+	int maxPoint = numberOfDivisions + 1;
+
+	glm::vec3 previousPoint = getCurvepointAt(0);
+
+	float sum = 0;
+
+	m_arcLengths.clear();
+	m_arcLengths.push_back(0.0);
+
+	for (int i = 1; i <= maxPoint; i++){
+		glm::vec3 p = getCurvepointAt(i / maxPoint);
+
+		//calculate distance
+		glm::vec3 temp = previousPoint - p;
+		float distSqr = dot(temp, temp);
+
+		sum += distSqr;
+
+		previousPoint = p;
+		m_arcLengths.push_back(sum);
+	};
+};
+
+glm::vec3 glannotations::BSpline::getCurvepointAt(float t) {
+	if ( t < 0.0f || t > 1.0f)
+		std::cout << "debug: warning! 0 <= t <= 1";
+
+	float tempT = t* m_ctrlPoints.size() - 1;
+	int i = std::floor(tempT) + m_degree - 1;
+	glm::vec3 point = deBoor(m_degree, m_degree, i, tempT, m_knotValues);
+
+	return point;
+};
+
+//not sure if the use of "inline" is correct here
+inline float clamp(float value, float min, float max){
+	//because there is no clamp in std
+	return value < min ? min : (value > max ? max : value);
+};
+
+glm::vec3 glannotations::BSpline::deBoor(int k, int degree, int i, float t, std::vector<float> knots) {
+	if (k == 0) {
+		i = clamp(i, 0.0f, m_ctrlPoints.size() - 1);
+		return m_ctrlPoints[i];
+	}
+	else {
+		float alpha;
+		try {
+			alpha = (t - knots[i]) / (knots[i + degree + 1 - k] - knots[i]);
+		} catch (...) {
+			alpha = 1.0f;
+			std::cout << "debug: alpha problem";
+		}
+
+		return (deBoor(k - 1, degree, i - 1, t, knots) * (1.0f - alpha) + (deBoor(k - 1, degree, i, t, knots) * (alpha)));
+	}
+};
+
+glm::vec2 glannotations::BSpline::getSecantVectorAt(float t, float nextT) {
+	glm::vec3 currentPoint = getCurvepointAt(t);
+	glm::vec3 nextPoint = getCurvepointAt(nextT);
+	glm::vec2 secantVector(nextPoint - currentPoint);
+
+	return secantVector;
+};
